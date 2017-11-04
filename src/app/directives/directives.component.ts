cd1895293b5b1c009c4296196f7d96e3c3eb95f7
Abhishek Gautam <099abhi@gmail.com>
2017-11-04 23:58:04 +0530

    Add files via upload

---
+++ src/app/directives/directives.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css']
+})
+export class DirectivesComponent implements OnInit {
+  numbers = [1, 2, 3, 4, 5];
+  odnum = [1, 3, 5];
+  evnum = [2, 4];
+  oddnum ;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
